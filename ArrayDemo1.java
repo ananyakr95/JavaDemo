ce9abbd52e20d10d0361ec85f1ca2b5a90390fec
ananyakhajuria <ananya.khajuria@active.ai>
2019-06-09 00:46:17 +0530

    ArrayDemo1

---
+++ ArrayDemo1.java
@@ -0,0 +1,23 @@
+
+
+
+class ArrayDemo1{
+
+
+	public int [] arrayLength(int len){
+
+		int [] arr = new int [len];
+
+    	return arr;
+    }
+
+     public static void main(String[] args){
+
+     	ArrayDemo1 obj = new ArrayDemo1();
+     
+		int [] arrDisp = obj.arrayLength(5);
+        
+
+      
+	}
+}
