7124eda6af1817265a995ea372517839c2514c4b
ananyakhajuria <ananya.khajuria@active.ai>
2019-06-09 01:09:16 +0530

    StudentArray Object

---
+++ Student.java
@@ -0,0 +1,17 @@
+class Student{
+
+	int rollNum;
+	int marks;
+	String name;
+
+	Student(int rollNum, int marks, String name) 
+    { 
+        this.rollNum = rollNum; 
+        this.marks = marks;
+        this.name = name; 
+    } 
+
+    
+    
+
+}
